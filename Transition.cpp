3d3f047f244c254af27f93909885911449e144cc
vemad <vincent.emad@insa-lyon.fr>
2015-03-18 11:38:20 +0100

    Ajout transitions

--- Transition.cpp
+++ Transition.cpp
@@ -12,7 +12,21 @@ Transition::Transition() {
 
 }
 
-Transition::~Transition() {
-	// TODO Auto-generated destructor stub
+Transition::~Transition() {}
+
+void DeplacerTrans::doTransition(Transition::TableTransition & tableTrans, std::stack<Etat> * PileEtats){
+
+PileEtats->push(cible);
+}
+
+void ReduireTrans::doTransition(Transition::TableTransition & tableTrans, std::stack<Etat> * PileEtats){
+
+	for(int i=0;i < depile; i++){
+
+		PileEtats->pop();
+
+		tableTrans.find(PileEtats->top())->second.find(precedent)->second->doTransition(tableTrans, PileEtats);
+	}
+
 }
 
--- Transition.h
+++ Transition.h
@@ -18,8 +18,32 @@ class Transition {
 public:
 	 typedef std::map<Etat::Id, std::map<Symbole::ident, Transition *>> TableTransition;
 
-	Transition();
+	 virtual void doTransition(Transition::TableTransition & tableTrans, std::stack<Etat> * PileEtats) = 0;
 	virtual ~Transition();
 };
 
+class DeplacerTrans: public Transition {
+public:
+
+	DeplacerTrans(Etat::Etat uneCible): cible(cible){}
+
+	void doTransition(Transition::TableTransition & tableTrans, std::stack<Etat> * PileEtats);
+
+protected:
+	Etat::Etat cible;
+
+
+};
+
+class ReduireTrans: public Transition{
+public:
+
+	ReduireTrans(int depile):depile(depile){}
+
+	void doTransition(Transition::TableTransition & tableTrans, std::stack<Etat> * PileEtats);
+
+protected:
+	int depile;
+	Symbole::ident precedent;
+};
 #endif /* TRANSITION_H_ */
