1a8fdfc9894c9a9be9d114789da49f345086613a
Huber <huber.gerard1@gmail.com>
2015-03-18 09:36:14 +0100

    Ajout de ID_NAMES

--- Symbole.cpp
+++ Symbole.cpp
@@ -8,6 +8,17 @@
 #include "Symbole.h"
 
 
+static const std::map<Symbole::Id, std::string> ID_NAMES = {
+		{Symbole::D, "D"}, {Symbole::r, "lire"}, {Symbole::aff, ":="},
+		{Symbole::plus, "+"}, {Symbole::moins, "-"}, {Symbole::cst, "const"},
+		{Symbole::var, "var"}, {Symbole::parf, ")"}, {Symbole::pv, ";"},
+		{Symbole::egal, "="}, {Symbole::w, "ecrire"}, {Symbole::D, "D"},
+		{Symbole::multi, "*"}, {Symbole::divi, "/"},{Symbole::paro, "("},
+		{Symbole::virg, ","}, {Symbole::var, ","}, {Symbole::nb, "num"},
+		{Symbole::P, "P"}, {Symbole::Lv, "Lv"}, {Symbole::Lc, "Lc"},
+		{Symbole::Li, "Li"}, {Symbole::I, "I"}, {Symbole::E, "E"}
+
+};
+
 Symbole::~Symbole() {
 }
-
