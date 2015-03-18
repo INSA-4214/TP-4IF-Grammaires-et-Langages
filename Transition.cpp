d492e53ade80f92bb71bc4eb9cbabfe245b33804
vemad <vincent.emad@insa-lyon.fr>
2015-03-18 09:04:04 +0100

    Ajout Transition

---
+++ Transition.cpp
@@ -0,0 +1,18 @@
+/*
+ * Transition.cpp
+ *
+ *  Created on: 18 mars 2015
+ *      Author: Vincent
+ */
+
+#include "Transition.h"
+
+Transition::Transition() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Transition::~Transition() {
+	// TODO Auto-generated destructor stub
+}
+
