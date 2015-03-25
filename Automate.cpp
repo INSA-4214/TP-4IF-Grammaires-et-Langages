af57c52e86222add51858ccb38b6a3b67b73b154
vemad <vincent.emad@insa-lyon.fr>
2015-03-25 11:45:51 +0100

    update automate.cpp

--- Automate.cpp
+++ Automate.cpp
@@ -11,7 +11,38 @@ Automate::~Automate() {
 
 }
 
-Automate::Automate() {
+Automate::Automate(std::istream * stream) {
 
+	lexer = new Lexer(stream);
+	//TODO init pileEtat
+	accepte = false;
 
+}
+
+void Automate::decalage(Symbole s, Etat e){
+
+	this->pileEtats.push(e);
+	this->pileSymboles.push(s);
+
+}
+bool Automate::lecture(){
+	//TODO get Token de la pile
+	//TODO boucle exploration
+	lexer.shift();
+	if( pileEtats.empty() )
+		return false;
+	return accepte;
+}
+
+std::stack<Etat> Automate::getPileEtats(){
+
+	return this->pileEtats;
+}
+std::stack<Symbole> Automate::getPileSymboles(){
+
+	return this->pileSymboles;
+}
+
+void Automate::setAccepter(bool b){
+	this->accepte = b;
 }
