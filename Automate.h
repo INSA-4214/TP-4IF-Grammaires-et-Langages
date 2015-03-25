0de2ccf8532a2febe5642dd767f50b640d9fe8b4
vemad <vincent.emad@insa-lyon.fr>
2015-03-25 11:38:00 +0100

    Update Automate

--- Automate.h
+++ Automate.h
@@ -10,21 +10,34 @@
 
 #include "Symbole.h"
 #include "Etat.h"
+#include "Lexer.h"
 #include "Transition.h"
 #include <stack>
-
+#include <string>
 class Automate {
 
 public:
-	Automate();
+	Automate(std::istream * stream);
+	bool lecture();
+	void decalage(Symbole s,Etat e);
+	std::stack<Etat> getPileEtats();
+	std::stack<Symbole> getPileSymboles();
+	void setAccepter(bool b);
 	virtual ~Automate();
-	void lecture();
 
-private:
+protected:
 
-//	std::stack<Etat> etats;
-	std::stack<Symbole> symboles;
-//	Transition::TableTransition transitions;
+/*La classe automate est dÃ©fini par :
+-une pile d'etats
+-une pile de symbole (token)
+-un analyseur lexical (lexer)
+-un etat d'avancement (valider/refuser l'analyse)
+*/
+
+	std::stack<Etat> pileEtats;
+	std::stack<Symbole> pileSymboles;
+	bool accepte;
+	Lexer lexer;
 
 };
 
