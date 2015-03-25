8963bd42dde3d46505198a634c4ed2d5d69a67ea
thomas-helmer <thomas.helmer69@gmail.com>
2015-03-25 11:43:15 +0100

    amelioration etat

--- Etats/Etat01.cpp
+++ Etats/Etat01.cpp
@@ -5,28 +5,11 @@
 Etat01::Etat01() : Etat() { }
 
 bool Etat01::transition(Automate *automate, Symbole * s) {
-   // Symbole symb = Symbole(Bd);
+    Symbole symb = Symbole(Symbole::Bd);
   /*  switch ( s->getIdent() ) {
-        case TokenType::I:
+        case Symbole::I:
             automaton->decalage(t, new E2());
             return true;
-        case TokenType::VAR:
-            automaton->decalage(t, new E21());
-            return true;
-        case TokenType::CONST:
-            automaton->decalage(t, new E27());
-            return true;
-        case TokenType::ID:
-        case TokenType::VAL:
-        case TokenType::V:
-        case TokenType::PV:
-        case TokenType::AFF:
-        case TokenType::EQ:
-        case TokenType::ENDOFFILE:
-        case TokenType::WRITE:
-        case TokenType::READ:
-            // Reduction N°12 - 0 Level Pop - "I->."
-            symb = Symbole(TokenType::I);
             if (!automaton->getStackStates()->top()->transition(
             automaton, &symb)) return false;
             if (!automaton->getStackStates()->top()->transition(
