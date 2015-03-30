384970dedc2a60d2fb67d74b262d208b423f383a
thomas-helmer <thomas.helmer69@gmail.com>
2015-03-30 23:42:26 +0200

    compile encore

--- Lexer.h
+++ Lexer.h
@@ -32,7 +32,7 @@ class Lexer
          * Regarde le prochain symbole terminal sans déplacer la tête de lecture
          */
         bool analyze();
-
+        int * getCurrPos();
         Symbole *getSymboleCourant();
 
     private:
@@ -44,6 +44,8 @@ class Lexer
         std::string m_debutTexte;
         std::string m_symboleCourantStr;
         Symbole * m_symboleCourant;
+        int *m_pos = new int [2];
+
 };
 
 #endif // LEXER_H
