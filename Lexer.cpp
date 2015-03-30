39a5556693cde5523b62a91b3cc58ae8c0b22c4a
Romain Lambert <rom93@free.fr>
2015-03-31 00:19:39 +0200

    Correction bug

--- Lexer.cpp
+++ Lexer.cpp
@@ -75,7 +75,7 @@ bool Lexer::analyze()
     	m_symboleCourantStr = matches[1];
         m_symboleCourant = new Symbole(Symbole::nb, m_symboleCourantStr);
     }
-    else
+    else if (m_symboleCourant->getIdent() != Symbole::FILEEND)
     {
     	m_symboleCourantStr = m_str.substr(0, 1);
         cout << "Caractere ignore : \"" << m_symboleCourantStr << "\"" << endl;
@@ -89,17 +89,20 @@ bool Lexer::analyze()
     return true;
 }
 
-void Lexer::shift() {
+void Lexer::shift(bool shift) {
 
 	if (!hasNext()) {
     	return;
     }
 
-    m_str=m_str.substr(m_symboleCourantStr.size());
-    m_str.erase(0, m_str.find_first_not_of(" \t\n\r\f\v"));
+    if (shift)
+    {
+        m_str=m_str.substr(m_symboleCourantStr.size());
+        m_str.erase(0, m_str.find_first_not_of(" \t\n\r\f\v"));
+    }
 
     // Si un caractère n'est pas reconnu, on l'efface et on passe à la suite
-    while (!analyze() && m_str.size() > 0) {
+    while (!analyze() && m_str != "") {
         m_str=m_str.substr(m_symboleCourantStr.size());
         m_str.erase(0, m_str.find_first_not_of(" \t\n\r\f\v"));
     }
