f8bcfa587c0aaea0906ddd7af5e64e6f1b52305e
Romain Lambert <rom93@free.fr>
2015-03-31 00:22:33 +0200

    Autre test de correction

--- Lexer.h
+++ Lexer.h
@@ -26,26 +26,28 @@ class Lexer
         /**
          * Décale la tête de lecture d'un mot vers la droite
          */
-        void shift();
+        void shift(bool shift);
 
         /**
          * Regarde le prochain symbole terminal sans déplacer la tête de lecture
          */
         bool analyze();
+
         int * getCurrPos();
+
         Symbole *getSymboleCourant();
 
     private:
 
-        bool hasNext();
         void parse(std::string *filename);
+        bool hasNext();
+
         std::string m_str;
         std::string m_texteEntier;
         std::string m_debutTexte;
         std::string m_symboleCourantStr;
         Symbole * m_symboleCourant;
         int *m_pos = new int [2];
-
 };
 
 #endif // LEXER_H
